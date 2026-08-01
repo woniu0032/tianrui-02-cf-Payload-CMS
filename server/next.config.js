dc8aaa9c4a5e19a570c6e951ae9309ec0ba3ecd1
nick0426002336 <wzjllzy@163.com>
2026-08-01 16:28:18 +0800

    fix: add outputFileTracingRoot to next.config.js for workspace detection

--- server/next.config.js
+++ server/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 import { withPayload } from '@payloadcms/next/withPayload'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-export default withPayload({})
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default withPayload({
+  outputFileTracingRoot: __dirname,
+})
